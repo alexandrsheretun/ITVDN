d71cce47fc89ab1786f68a89b4c80b53e2f149f1
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-18 17:43:34 +0300

    finished work on the ConAppThreeVariables program

--- Starter/VariablesAndDataTypes/ConAppThreeVariablesInt/Program.cs
+++ Starter/VariablesAndDataTypes/ConAppThreeVariablesInt/Program.cs
@@ -6,7 +6,19 @@ namespace ConAppThreeVariablesInt
     {
         static void Main(string[] args)
         {
-            
+            int x = 10, y = 12, z = 3;
+
+            int result1 = x += y - x++ * z;
+            Console.WriteLine("result of the first equation = {0}", result1);
+            int result2 = z = --x - y * 5;
+            Console.WriteLine("result of the second equation = {0}", result2);
+            int result3 = y /= x + 5 % z;
+            Console.WriteLine("result of the third equation = {0}", result3);
+            int result4 = z = x++ + y * 5;
+            Console.WriteLine("result of the fourth equation = {0}", result4);
+            int result5 = x = y - x++ * z;
+            Console.WriteLine("result of the fifth equation = {0}", result5);
+            Console.ReadKey();
         }
     }
 }
