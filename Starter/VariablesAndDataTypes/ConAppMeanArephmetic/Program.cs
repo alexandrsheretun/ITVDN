f6b225c4ef6c0a88351123f940aa4f6ce1227105
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-18 18:00:28 +0300

    finished work on the ConAppMeanArephmetic program

--- Starter/VariablesAndDataTypes/ConAppMeanArephmetic/Program.cs
+++ Starter/VariablesAndDataTypes/ConAppMeanArephmetic/Program.cs
@@ -6,7 +6,11 @@ namespace ConAppMeanArephmetic
     {
         static void Main(string[] args)
         {
-           
+            double a = 10, b = 14, c = 2;
+            double result = (a + b + c) / 3;
+            Console.WriteLine("the mean arephmetic of three numbers is = {0} ", result);
+            Console.ReadKey();
+
         }
     }
 }
