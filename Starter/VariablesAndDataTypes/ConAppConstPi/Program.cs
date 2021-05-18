b9567efc2c5ee53cd5c0e2a623cc3eb645017506
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-18 18:02:25 +0300

    start work on the ConAppConstPi program

---
+++ Starter/VariablesAndDataTypes/ConAppConstPi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConAppConstPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
