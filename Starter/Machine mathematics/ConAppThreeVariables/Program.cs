214f2821970f56f77bee7e2a27ceed6e37979a6d
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-18 15:09:00 +0300

    finished work on the ConAppThreeVariables program

--- Starter/Machine mathematics/ConAppThreeVariables/Program.cs
+++ Starter/Machine mathematics/ConAppThreeVariables/Program.cs
@@ -6,7 +6,10 @@ namespace ConAppThreeVariables
     {
         static void Main(string[] args)
         {
-            
-        }
+         Console.WriteLine("\nmy string 1");
+         Console.WriteLine("\tmy string 2");
+         Console.WriteLine("\amy string 3");
+         Console.ReadKey();
+         }
     }
 }
