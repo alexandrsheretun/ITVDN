655a74dcb89163155066137ffca96713a1a127ac
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-18 14:40:01 +0300

    finished work on the WPFApp 8 Button program

--- Starter/Machine mathematics/WPFApp 8 Button/MainWindow.xaml.cs
+++ Starter/Machine mathematics/WPFApp 8 Button/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFApp_8_Button
 {
@@ -28,6 +15,41 @@ namespace WPFApp_8_Button
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("0 : 255","byte");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("-128 : +127", "sbyte");
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("0 : 65535", "ushort");
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("-32768 : +32767","short");
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("0u : 4294967295U","uint");
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("-2147483648 : +2147483647","int");
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("0ul : 18446744073709551615UL","ulong");
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("-9223372036854775808L : +9223372036854775807L","long");
         }
     }
 }
