dfca625237ddf40cc4653325ed391d545cb38f30
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-18 14:57:18 +0300

    finished work on the ConApp pi program

--- Starter/Machine mathematics/ConApp pi/Program.cs
+++ Starter/Machine mathematics/ConApp pi/Program.cs
@@ -6,12 +6,12 @@ namespace ConApp_pi
     {
         static void Main(string[] args)
         {
-            
+            double pi = 3.141592653;
+            Console.WriteLine("pi is = {0}", pi);
 
-
-
-
-
+            decimal e = 2.7182818284590452M;
+            Console.WriteLine("e is = {0}", e);
+            Console.ReadKey();
         }
     }
 }
