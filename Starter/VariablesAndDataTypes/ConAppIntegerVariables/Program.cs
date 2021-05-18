b784fc624a26f26db60d9c5298e59d0ac8419fd7
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-18 17:25:02 +0300

    finished work on the ConAppIntegerVariables program

--- Starter/VariablesAndDataTypes/ConAppIntegerVariables/Program.cs
+++ Starter/VariablesAndDataTypes/ConAppIntegerVariables/Program.cs
@@ -6,7 +6,28 @@ namespace ConAppIntegerVariables
     {
         static void Main(string[] args)
         {
-           
+            int a = 10, b = 5;
+            int result = a + b;
+            Console.WriteLine("press any button to add");
+            Console.ReadKey();
+            Console.WriteLine("a + b = {0}", result);
+
+            int result2 = a - b;
+            Console.WriteLine("press any button to subtract");
+            Console.ReadKey();
+            Console.WriteLine("a - b = {0}", result2);
+
+            int result3 = a / b;
+            Console.WriteLine("press any button to split");
+            Console.ReadKey();
+            Console.WriteLine("a / b = {0}", result3);
+
+            int result4 = a * b;
+            Console.WriteLine("press any button to multiply");
+            Console.ReadKey();
+            Console.WriteLine("a * b = {0}", result4);
+
+            Console.ReadKey();
         }
     }
 }
