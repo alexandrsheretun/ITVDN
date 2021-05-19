208e32a1f9f84fcd4a3e1cc58ce4dd4e590f5b18
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-19 16:42:21 +0300

    finished work on the ConAppConstPi program

--- Starter/VariablesAndDataTypes/ConAppConstPi/Program.cs
+++ Starter/VariablesAndDataTypes/ConAppConstPi/Program.cs
@@ -6,7 +6,13 @@ namespace ConAppConstPi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const double pi = 3.14;
+            Console.WriteLine("set the number R");
+            var R = Convert.ToDouble(Console.ReadLine());
+            double result = pi * Math.Pow(R, 2);            
+            Console.WriteLine("The area of the circle is = {0}", result);
+            Console.ReadKey();
+
         }
     }
 }
