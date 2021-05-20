f939084b1f9220e73538c61d6a6674f3a6b87d82
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-20 13:24:20 +0300

    finished work on the ConAppDefinition program

--- Starter/Сonditional constructions/ConAppDefinition/Program.cs
+++ Starter/Сonditional constructions/ConAppDefinition/Program.cs
@@ -6,12 +6,33 @@ namespace ConAppDefinition
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter a number between 0 and 100");
+            var userValue = Convert.ToInt32(Console.ReadLine());
 
+            if (userValue >= 0 && userValue <= 14)
+            {
+                Console.WriteLine("You are in the first numerical range");
+            }
 
+            else if (userValue >= 15 && userValue <= 35)
+            {
+                Console.WriteLine("You are in the second numerical range");
+            }
 
+            else if (userValue >= 36 && userValue <= 50)
+            {
+                Console.WriteLine("You are in the third numerical range");
+            }
+            else if (userValue >= 50 && userValue <= 100)
+            {
+                Console.WriteLine("You are in the fourth numerical range");
+            }
+            else
+            {
+                Console.WriteLine("Error. Enter a value from 0 to 100");
+            }
 
-
+            Console.ReadKey();
         }
     }
 }
