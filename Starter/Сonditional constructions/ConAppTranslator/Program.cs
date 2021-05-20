2f1b9b2be328f5e58f02f6e4efc4dfc62a8f6532
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-20 12:21:59 +0300

    finished work on the ConAppTranslator program

--- Starter/Сonditional constructions/ConAppTranslator/Program.cs
+++ Starter/Сonditional constructions/ConAppTranslator/Program.cs
@@ -6,7 +6,61 @@ namespace ConAppTranslator
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter a word in Russian : ");
+            string weather = Console.ReadLine();
+            string result = null;
+
+            switch (weather)
+            {
+                case "пасмурно":
+                    {
+                        result = "Mainly cloudy";
+                        break;
+                    }
+                case "солнечно":
+                    {
+                        result = "sunny";
+                        break;
+                    }
+                case "дождливо":
+                    {
+                        result = "rainy";
+                        break;
+                    }
+                case "ветренно":
+                    {
+                        result = "windy";
+                        break;
+                    }
+                case "тумано":
+                    {
+                        result = "foggy";
+                        break;
+                    }
+                case "облачно":
+                    {
+                        result = "cloudy";
+                        break;
+                    }
+                case "снежно":
+                    {
+                        result = "snowy";
+                        break;
+                    }
+                case "засуха":
+                    {
+                        result = "drought";
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("В переводчике нету слова для перевода. Попробуйте ввести другое слово!");
+                        break;
+                    }
+
+            }
+            Console.WriteLine("Translate : {0}", result);
+            Console.ReadKey();
         }
     }
 }
