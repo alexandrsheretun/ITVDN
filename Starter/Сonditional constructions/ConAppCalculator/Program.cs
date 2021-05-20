51c9cda2bb5c7832f1c473a5ced61d78c8c67ca9
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-20 12:40:04 +0300

    finished work on the ConAppCalculator program

--- Starter/Сonditional constructions/ConAppCalculator/Program.cs
+++ Starter/Сonditional constructions/ConAppCalculator/Program.cs
@@ -6,8 +6,57 @@ namespace ConAppCalculator
     {
         static void Main(string[] args)
         {
-           
-                
+            double operand1 = 10, operand2 = 2, result = 0;
+
+            Console.WriteLine("Enter the sign of the arithmetic operation: '+', '-', '*', '/', '%'");
+
+            string sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "+":
+                    {
+                        result = operand1 + operand2;
+                        break;
+                    }
+                case "-":
+                    {
+                        result = operand1 - operand2;
+                        break;
+                    }
+                case "*":
+                    {
+                        result = operand1 * operand2;
+                        break;
+                    }
+                case "/":
+                    {
+                        if (operand2 == 0)
+                        {
+                            Console.WriteLine("Сannot be divided by zero! Enter a value greater than 0 in operand2");
+                        }
+                        else
+                        {
+                            result = operand1 / operand2;
+                        }
+                        break;
+                    }
+                case "%":
+                    {
+                        result = operand1 % operand2;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Such an arithmetic operation does not exist!");
+                        break;
+                    }
+            }
+            Console.WriteLine("The result of your arithmetic operation:" + result);
+            Console.ReadKey();
+
         }
+
+        
     }
 }
