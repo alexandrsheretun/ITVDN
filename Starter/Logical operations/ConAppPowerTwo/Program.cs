7710b6f585e2557ed3a7ac145d5aeb8f444fd8e3
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-24 14:43:47 +0300

    finished work on the ConAppPowerTwo program

--- Starter/Logical operations/ConAppPowerTwo/Program.cs
+++ Starter/Logical operations/ConAppPowerTwo/Program.cs
@@ -6,7 +6,17 @@ namespace ConAppPowerTwo
     {
         static void Main(string[] args)
         {
-           
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (Step(a)) Console.WriteLine("The number - {0} is a power of two!", a);
+            else Console.WriteLine("The number - {0} is not a power of two!", a);
+            Console.ReadKey();
         }
+        public static bool Step(int a)
+        {
+            if (a == 2) return true;
+            else if (a % 2 == 0) return Step(a / 2);
+            else return false;
+        }
+    
     }
 }
