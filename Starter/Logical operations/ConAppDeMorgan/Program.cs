d1465d6fb724b0fe04c25ae02ff9a7c37f05597c
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-24 14:48:15 +0300

    finished work on the ConAppDeMorgan program

--- Starter/Logical operations/ConAppDeMorgan/Program.cs
+++ Starter/Logical operations/ConAppDeMorgan/Program.cs
@@ -6,7 +6,20 @@ namespace ConAppDeMorgan
     {
         static void Main(string[] args)
         {
-            
+            bool A = false;
+            bool B = true;
+            //condition before applying de Morgan's theorem
+            if (A | B)
+            {
+                Console.WriteLine("(A | B) = {0}", (A | B));
+            }
+            //condition after applying de Morgan's theorem
+            if (!(!A & !B))
+            {
+                Console.WriteLine("!(!A & !B) = {0}", !(!A & !B));
+            }
+
+            Console.ReadKey();
         }
     }
 }
