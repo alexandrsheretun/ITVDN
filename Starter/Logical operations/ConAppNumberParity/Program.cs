514a69f04758d99fa5f8a65b2cddec53989f2e11
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-24 15:55:31 +0300

    finished work on the ConAppNUmberParity program

--- Starter/Logical operations/ConAppNumberParity/Program.cs
+++ Starter/Logical operations/ConAppNumberParity/Program.cs
@@ -6,7 +6,29 @@ namespace ConAppNumberParity
     {
         static void Main(string[] args)
         {
-            
+            //first option
+            Console.Write("Enter your number:");
+            var value = Convert.ToInt32(Console.ReadLine());
+            if ((value % 2) == 0)
+            {
+                Console.WriteLine("You entered {0}, it's even number", value);
+            }
+            else
+            {
+                Console.WriteLine("You entered {0}, it's not even number", value);
+            }
+            //second option
+
+            if ((value & 1) == 0)
+            {
+                Console.WriteLine("You entered {0}, it's even number", value);
+            }
+            else
+            {
+                Console.WriteLine("You entered {0}, it's not even number", value);
+            }
+
+            Console.ReadKey();
         }
     }
 }
