4c6942b089df8b6ab3ec4444bf8838cfa156d8d4
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-27 13:47:24 +0300

    finished work on the ConAppLogicalOperation program

--- Starter/Logical operations/ConAppLogicalOperation/Program.cs
+++ Starter/Logical operations/ConAppLogicalOperation/Program.cs
@@ -6,7 +6,32 @@ namespace ConAppLogicalOperation
     {
         static void Main(string[] args)
         {
-            
+            int x = 5, y = 10, z = 15;
+
+            {
+                //15 >> 75 = 
+                var result = x += y >> x++ * z;
+                Console.WriteLine("Result of the x mathematical expression: {0}", result);
+            }
+            {
+                //6 & 50 = 
+                var result = z = ++x & y * 5;
+
+                Console.WriteLine("Result of the z mathematical expression: {0}", result);
+            }
+            {
+                //7 | 15 = 
+                var result = y /= x + 5 | z;
+
+                Console.WriteLine("Result of the y mathematical expression: {0}", result);
+            }
+            {
+                //10 << 64 = 
+                var result = x = y << x++ ^ z;
+
+                Console.WriteLine("Result of the x mathematical expression {0}", result);
+            }
+            Console.ReadKey();
         }
     }
 }
