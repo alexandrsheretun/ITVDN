2b2b83d4bd40ecbf5cc7ed5d9b5d150c0e1145f0
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-05-29 12:55:26 +0300

    finished work on the ConAppRectangleLoop program

--- Starter/Cyclic constructions/ConAppRectangleLoop/Program.cs
+++ Starter/Cyclic constructions/ConAppRectangleLoop/Program.cs
@@ -6,7 +6,21 @@ namespace ConAppRectangleLoop
     {
         static void Main(string[] args)
         {
+            {// create loop for
 
+                for (int i = 0; i < 10; i++)
+                {//create nested loop for on body first loop for 
+                    for (int r = 0; r < 20; r++)
+                    {
+                        //display on screen
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+
+            }
         }
     }
 }
