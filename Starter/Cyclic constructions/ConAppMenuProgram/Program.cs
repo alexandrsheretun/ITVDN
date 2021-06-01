164ebfe82ba19ae1a39376c87259b4f7b1bfc4dc
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-06-01 15:03:20 +0300

    start work on the ConAppMenuProgram program

---
+++ Starter/Cyclic constructions/ConAppMenuProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConAppMenuProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
