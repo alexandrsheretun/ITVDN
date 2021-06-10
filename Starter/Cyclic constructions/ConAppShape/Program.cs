016be933e5114e7bcb68f4ef1745b0d031f9b523
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-06-10 12:56:53 +0300

    finished work on the ConAppShape program

--- Starter/Cyclic constructions/ConAppShape/Program.cs
+++ Starter/Cyclic constructions/ConAppShape/Program.cs
@@ -6,7 +6,29 @@ namespace ConAppShape
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int g = 24;
+
+            //Characters
+            int z = 2;
+            //8 lines
+            for (int i = 0; i < 8; i++)
+            {
+                //print in line
+                for (int j = 0; j < 52; j++)
+                {
+                    Console.Write(" ");
+                    if (j == g)
+                    {
+                        for (int s = 0; s < z; s++)
+                            Console.Write("*");
+                    }
+                }
+                g -= 1;
+                z += 2;
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
