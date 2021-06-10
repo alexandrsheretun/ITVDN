29d201650aff8352a1f4737493e2229ce9357d55
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-06-10 12:45:43 +0300

    start work on the ConAppSumOddLoop program

--- Starter/Cyclic constructions/ConAppMenuProgram/Program.cs
+++ Starter/Cyclic constructions/ConAppMenuProgram/Program.cs
@@ -6,7 +6,40 @@ namespace ConAppMenuProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            
+            Console.WriteLine("###Menu###");
+            Console.WriteLine("View");
+            Console.WriteLine("Info");
+            Console.WriteLine("Edit");
+            Console.Write("\n" + "Change your key : ");
+            var ch = Console.ReadKey().Key;
+            while(ch != ConsoleKey.Escape);
+                switch (ch)
+                {
+                    case ConsoleKey.RightArrow:
+                        {
+                            Console.WriteLine("View");
+                            break;
+                        }
+
+                    case ConsoleKey.LeftArrow:
+                        {
+                            Console.WriteLine("Info");
+                            break;
+                        }
+                    case ConsoleKey.UpArrow:
+                        {
+                            Console.WriteLine("Edit");
+                            break;
+                        }
+                    case ConsoleKey.Escape:
+                        {
+                            Console.WriteLine("end program");
+                            break;
+                        }
+                }
+                
         }
     }
+
 }
---
+++ Starter/Cyclic constructions/ConAppSumOddLoop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConAppSumOddLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
