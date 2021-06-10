db76fa8ce61ee22b711816ed5c8393efcff01268
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-06-10 12:51:18 +0300

    start work on the ConAppShape program

---
+++ Starter/Cyclic constructions/ConAppShape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConAppShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
