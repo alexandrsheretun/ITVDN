2205f6a983384ed3e4966a373ecc7ffe512a0cf6
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-06-10 12:48:55 +0300

    finished work on the ConAppSumOddLoop program

--- Starter/Cyclic constructions/ConAppSumOddLoop/Program.cs
+++ Starter/Cyclic constructions/ConAppSumOddLoop/Program.cs
@@ -6,7 +6,32 @@ namespace ConAppSumOddLoop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input first number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input second number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            //sum values ​​between numbers indicated
+            for (int i = a + 1; i < b; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("The sum of all numbers located between the indicated numbers - {0}", sum);
+
+            //odd values ​​between numbers indicated
+            for (int i = a + 1; i < b; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.Write("Odd values between numbers indicated:");
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
