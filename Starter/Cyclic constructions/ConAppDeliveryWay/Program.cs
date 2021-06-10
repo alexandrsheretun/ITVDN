77c702d345b6bd10473e522a02634fd6dbe32e14
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-06-10 13:01:02 +0300

    finished work on the ConAppDeliveryWay program

--- Starter/Cyclic constructions/ConAppDeliveryWay/Program.cs
+++ Starter/Cyclic constructions/ConAppDeliveryWay/Program.cs
@@ -6,7 +6,31 @@ namespace ConAppDeliveryWay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //display on screen string
+            Console.Write("Enter the number of customers for delivery: ");
+            //input user numeral
+            int counter = Convert.ToInt32((Console.ReadLine()));
+            int factorial = 1;
+            //negative check
+            if (counter < 0)
+            {   //Error - negative check
+                Console.WriteLine("The number of customers cannot be negative. Try again!");
+            }
+            else
+            {
+                do
+                {
+                    factorial *= counter--;
+                }
+
+                while (counter > 0);
+
+                {   //display on screen result
+                    Console.Write("Possible delivery routes: {0}", factorial);
+                }
+            }
+            //Delay
+            Console.ReadKey();
         }
     }
 }
