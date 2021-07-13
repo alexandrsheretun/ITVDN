00f3784d31d62c288627aa0019c4ac02d56dba21
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-07-13 17:38:10 +0300

    finished work on the MethodCalculate program

---
+++ Starter/Methods/ConAppMethodCalculate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConAppMethodCalculate
+{   
+        class Program
+        {
+            //create method Calculate - signature and body
+            static int Calculate(int first, int second, int third)
+            {
+                //return the value of the method arguments
+                return (first + second + third) / 2;
+            }
+
+            static void Main()
+            {  //set the value to our variables
+                int first = 2, second = 3, third = 5;
+                int result = Calculate(first, second, third);
+                Console.WriteLine("Arithmetic mean of argument values: {0}", result);
+                Console.Write("Press any key to close console window");
+                Console.ReadKey();
+
+            }
+        }
+    
+}
