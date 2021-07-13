c0094ce6d827c2a07efaff88301f0052ee07a4c4
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-07-13 19:17:26 +0300

    finished work on the MethodsArithmetics program

--- Starter/Methods/MethodsArithmeticsProgram/Program.cs
+++ Starter/Methods/MethodsArithmeticsProgram/Program.cs
@@ -4,9 +4,88 @@ namespace MethodsArithmeticsProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        // method Add
+        static int Add(int operand1, int operand2)
         {
-            Console.WriteLine("Hello World!");
+            return operand1 + operand2;
+        }
+        // method Sub
+        static int Sub(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+        // method Mul
+        static int Mul(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+        // method Dev
+        static int Dev(int operand1, int operand2)
+        {
+            return operand1 / operand2;
+        }
+        // method Sub
+        static void Main()
+
+        {   //input value1,convert to type Int32
+            Console.Write("Введите значение операнда 1: ");
+            string value1 = Console.ReadLine();
+            int number1 = Convert.ToInt32(value1);
+
+            //input arithmetic operations
+            Console.Write("Допустимые арифметические операции: '+', '-', '*', '/'\nВведите символ арифметической операции: ");
+            string symbol = Console.ReadLine();
+
+            //input value2,convert to type Int32
+            Console.Write("Введите значение операнда 2: ");
+            string value2 = Console.ReadLine();
+            int number2 = Convert.ToInt32(value2);
+
+            //used construction SwitchCase
+            switch (symbol)
+            {
+                //if user input symbol "+"
+                case "+":
+                    {
+                        int result = Add(number1, number2);
+                        Console.Write("Результат: {0} + {1} = {2}", number1, number2, result);
+                        break;
+                    }
+
+                //if user input symbol "-"
+                case "-":
+                    {
+                        int result = Sub(number1, number2);
+                        Console.Write("Результат: {0} - {1} = {2}", number1, number2, result);
+                        break;
+                    }
+                //if user input symbol "*"
+                case "*":
+                    {
+                        int result = Mul(number1, number2);
+                        Console.Write("Результат: {0} * {1} = {2}", number1, number2, result);
+                        break;
+                    }
+                //if user input symbol "/"
+                case "/":
+                    {
+                        //check division by zero
+                        if (number2 == 0)
+                        {
+                            Console.Write("Warning! Division by zero. Please, input other value!");
+                            break;
+                        }
+                        else
+                        {
+                            int result = Dev(number1, number2);
+                            Console.Write("Результат: {0} / {1} = {2}", number1, number2, result);
+                            break;
+                        }
+                    }
+            }
+            //Delay
+            Console.ReadKey();
+
         }
     }
 }
