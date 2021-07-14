4e488198d30037001a45c41430ff503f1ddff0a1
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-07-14 14:59:37 +0300

    start work on the Positive / Negative Number Method program

---
+++ Starter/Methods/ConAppPositiveNegative Number Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConAppPositiveNegative_Number_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
