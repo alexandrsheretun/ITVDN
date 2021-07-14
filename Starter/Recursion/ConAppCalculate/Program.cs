7a658bc3a44781b804080ed1f83ac0f7e9858705
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-07-14 15:49:56 +0300

    finished work on the Calculate program

--- Starter/Recursion/ConAppCalculate/Program.cs
+++ Starter/Recursion/ConAppCalculate/Program.cs
@@ -4,9 +4,26 @@ namespace ConAppCalculate
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Calculate(ref int a, ref int b, ref int c)
         {
-            Console.WriteLine("Hello World!");
+            a /= 5;
+            b /= 5;
+            c /= 5;
+        }
+        static void Main()
+        {
+            Console.Write("Input A - ");
+            int operand1;
+            int.TryParse(Console.ReadLine(), out operand1);
+            Console.Write("Input B - ");
+            int operand2;
+            int.TryParse(Console.ReadLine(), out operand2);
+            Console.Write("Input C - ");
+            int operand3;
+            int.TryParse(Console.ReadLine(), out operand3);
+            Calculate(ref operand1, ref operand2, ref operand3);
+            Console.WriteLine("{0}, {1}, {2}", operand1, operand2, operand3);
+            Console.ReadKey();
         }
     }
 }
