ee9567a7acc9ab298d8beb0ec538d36e64421701
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-07-14 16:10:12 +0300

    finished work on the ConAppBank program

--- Starter/Recursion/ConAppBank/Program.cs
+++ Starter/Recursion/ConAppBank/Program.cs
@@ -4,9 +4,23 @@ namespace ConAppBank
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Credit(int pay)
         {
-            Console.WriteLine("Hello World!");
+            int debt = 700;
+            Console.WriteLine("The client paid {0} $ of debt", pay);
+            int debtCheck = debt - pay;
+            if (debtCheck < 0) Console.WriteLine("Prepayment is - {0} $", -debtCheck);
+            else if (debtCheck == 0) Console.WriteLine("All debt paid");
+            else Console.WriteLine("Debt is - {0} $", debtCheck);
+        }
+        static void Main()
+        {
+            Console.Write("Input amount of payment - ");
+            int payment;
+            int.TryParse(Console.ReadLine(), out payment);
+            Credit(payment);
+            Console.ReadKey();
         }
     }
 }
+
