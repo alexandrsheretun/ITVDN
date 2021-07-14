ec27e77c6f01d51328082df293312fb84d7afeab
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-07-14 15:15:28 +0300

    finished work on the Positive / Negative Number Method program

--- Starter/Methods/ConAppPositiveNegative Number Method/Program.cs
+++ Starter/Methods/ConAppPositiveNegative Number Method/Program.cs
@@ -4,9 +4,20 @@ namespace ConAppPositiveNegative_Number_Method
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Number(int x)
         {
-            Console.WriteLine("Hello World!");
+            //positive or negative check
+            Console.WriteLine(x >= 0 ? "You entered a positive number" : "You entered a negative number");
+        }
+        static void Main()
+        {
+            Console.Write("Enter the number: ");
+
+            string value = Console.ReadLine();
+            int userNum = Convert.ToInt32(value);
+            Number(userNum);
+            //delay
+            Console.ReadKey();
         }
     }
 }
