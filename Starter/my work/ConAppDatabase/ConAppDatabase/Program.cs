3990fc3ef5c3b565c720952bd6ccb96a666947c0
Alexandr.Sheretun@gmail.com <restart228maestro>
2021-07-17 21:55:44 +0300

    finished work on the ConAppDatabase program

---
+++ Starter/my work/ConAppDatabase/ConAppDatabase/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConAppDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 1, 3, 4, 5, 2, 1, -4, -1, 10, 55 };
+            //Block 1
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            //Block 2
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+            //Block 3
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = array[i];
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            //Block 4 
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                Console.WriteLine(array[i]);
+            }
+            //Block 5
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = array[i];
+                if (number == -1)
+                    break;
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
